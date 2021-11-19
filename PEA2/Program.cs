dc1ed5e66a9457169df25a44733c4e6c9cb33f89
Maciej Byczko <byczqq136@gmail.com>
2021-11-20 00:17:22 +0100

    Add diversification in menu

--- PEA2/Program.cs
+++ PEA2/Program.cs
@@ -16,12 +16,13 @@ internal static class InfoString
 
 
     public const string ManualMenu = "0.Exit\n" +
-                               "1.Read graph from file\n" +
-                               "2.Create random graph\n" +
-                               "3.Display graph\n" +
-                               "4 Save graph\n" +
-                               "5.Simulated Annealing\n" +
-                               "6.Tabu Search\n";
+                                     "1.Read graph from file\n" +
+                                     "2.Create random graph\n" +
+                                     "3.Display graph\n" +
+                                     "4 Save graph\n" +
+                                     "5.Turn ON/OFF Diversification\n" +
+                                     "6.Simulated Annealing\n" +
+                                     "7.Tabu Search\n";
 }
 
 namespace PEA2
@@ -32,12 +33,10 @@ namespace PEA2
         {
             ProgramMenu();
         }
-        
+
 
         private static void ProgramMenu()
         {
-            
-            
             var exit = false;
             while (!exit)
             {
@@ -68,8 +67,9 @@ namespace PEA2
         {
             Matrix matrix = null; // field for matrices
             var exit = false; // boolean for program work
-            var timer = new Stopwatch(); // set timer
-            var numberOfTrials = 1000;
+            // var timer = new Stopwatch(); // set timer
+            // var numberOfTrials = 1000;
+            var diversification = false;
             while (!exit)
             {
                 Console.WriteLine(InfoString.ManualMenu); // print menu
@@ -102,25 +102,31 @@ namespace PEA2
                         var filename = Console.ReadLine();
                         Essentials.SaveToFile(matrix, filename);
                         break;
-                    case 5: // Simulated Annealing Algorithm
+                    case 5: // diversification
+                        diversification = !diversification;
+                        Console.WriteLine(diversification ? "Diversification ON" : "Diversification OFF");
+
+                        break;
+                    case 6: // Simulated Annealing Algorithm
                         if (matrix == null)
                         {
                             Console.WriteLine(InfoString.ErrorNoMatrix);
                             break;
                         }
                         // var timer = new Stopwatch();
-                        
-                        timer.Restart();
-                        var bestPermutation = Algorithms.SimulatedAnnealing(matrix, numberOfTrials);
-                        timer.Stop();
+                        Console.Write("Write time for Annealing [s]: ");
+                        var trialTime = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                        // timer.Restart();
+                        var bestPermutation = Algorithms.SimulatedAnnealing(matrix, trialTime);
+                        // timer.Stop();
                         Console.WriteLine("best permutation:");
                         Essentials.DisplayArray(bestPermutation);
                         Console.WriteLine("Road Value:\n" + matrix.CalculateRoad(bestPermutation));
-                        Console.WriteLine("Time:\n" + Essentials.CalculateTimeMs(timer) + " ms");
+                        // Console.WriteLine("Time:\n" + Essentials.CalculateTimeMs(timer) + " ms");
 
                         Console.WriteLine("TODO Simulated Annealing");
                         break;
-                    case 6: // Tabu Search Algorithm
+                    case 7: // Tabu Search Algorithm
                         if (matrix == null)
                         {
                             Console.WriteLine(InfoString.ErrorNoMatrix);
