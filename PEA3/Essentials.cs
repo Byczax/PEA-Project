928964025420778ab2542b0647a6838eba9b9635
Maciej Byczko <byczqq136@gmail.com>
2021-12-07 08:54:57 +0100

    Add template for third project
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

---
+++ PEA3/Essentials.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PEA3
+{
+    public static class Essentials
+    {
+        public static Matrix ReadFile(string filename)
+        {
+            if (filename == null || !File.Exists(filename))
+            {
+                Console.WriteLine("ERROR, Wrong filename");
+                return null;
+            }
+
+            var lines = File.ReadAllLines(filename);
+            var matrix = new Matrix(int.Parse(lines[0]));
+            for (var i = 1; i <= int.Parse(lines[0]); i++)
+            {
+                var newLine = lines[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (var j = 0; j < int.Parse(lines[0]); j++) matrix.Array[i - 1, j] = int.Parse(newLine[j]);
+            }
+
+            return matrix;
+        }
+
+        public static Matrix GenerateRandomGraph(int size)
+        {
+            var rand = new Random();
+            var matrix = new Matrix(size);
+            for (var i = 0; i < size; i++)
+            for (var j = 0; j < size; j++)
+                if (i == j)
+                    matrix.Array[i, j] = 9999;
+                else
+                    matrix.Array[i, j] = rand.Next() % 100;
+            return matrix;
+        }
+
+        public static void DisplayList(List<int> list)
+        {
+            Console.Write(list[0]);
+            foreach (var element in list.Skip(1)) Console.Write(", " + element);
+
+            Console.WriteLine();
+        }
+
+        private static void DisplayArray(int[] array)
+        {
+            Console.Write(array[0]);
+            foreach (var element in array.Skip(1)) Console.Write(", " + element);
+
+            Console.WriteLine();
+        }
+
+        public static void DisplayFullArray(int[] array)
+        {
+            if (array == null)
+            {
+                Console.WriteLine("ERROR, EMPTY, something gone WRONG!");
+                return;
+            }
+
+            Console.Write(array[0]);
+            foreach (var element in array.Skip(1)) Console.Write(", " + element);
+
+            Console.WriteLine(", " + array[0]);
+        }
+
+        public static void Display0List(IEnumerable<int> list)
+        {
+            Console.Write("0, ");
+            foreach (var element in list) Console.Write(element + ", ");
+            Console.WriteLine("0");
+        }
+
+        private static double MeasureTime(Func<Matrix, IList<int>> algorithm, Matrix matrix)
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            algorithm(matrix);
+            timer.Stop();
+            return CalculateTimeMs(timer); // return ms
+        }
+
+        public static void MeasureAndPrint(Func<Matrix, int[]> algorithm, Matrix matrix)
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            var bestPermutation = algorithm(matrix);
+            timer.Stop();
+            Console.WriteLine("best permutation:");
+            DisplayArray(bestPermutation);
+            Console.WriteLine("Road Value:\n" + matrix.CalculateRoad(bestPermutation));
+            Console.WriteLine("Time:\n" + CalculateTimeMs(timer) + " ms");
+        }
+
+        private static double CalculateTimeMs(Stopwatch timer)
+        {
+            return (double)1000 * timer.ElapsedTicks / Stopwatch.Frequency;
+        }
+
+        public static double LoopAndSum(Func<Matrix, IList<int>> algorithm, Matrix matrix, int repetition)
+        {
+            double time = 0;
+            for (var i = 0; i <= repetition; i++)
+            {
+                if (i == 0) continue;
+                time += MeasureTime(algorithm, matrix);
+            }
+
+            return time / repetition / 1000;
+        }
+
+        public static void SaveToFile(Matrix matrix, string filename)
+        {
+            File.WriteAllText(filename, matrix.Size + "\n");
+            for (var i = 0; i < matrix.Size; i++)
+            {
+                for (var j = 0; j < matrix.Size; j++) File.AppendAllText(filename, matrix.Array[i, j] + " ");
+                File.AppendAllText(filename, "\n");
+            }
+        }
+    }
+}
---
+++ PEA3/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+
+internal static class InfoString
+{
+    public const string ErrorNoMatrix = "ERROR, No matrix created/loaded";
+    public const string GetFileName = "Write file name with extension: ";
+    public const string ErrorWrongChoice = "ERROR, Wrong choice, try again";
+    public const string Bye = "BYE!";
+
+    public const string Menu = "0.Exit\n" +
+                               "1.Manual testing\n" +
+                               "2.Automated tests\n";
+
+
+    public const string ManualMenu = "0.Exit\n" +
+                                     "1.Read graph from file\n" +
+                                     "2.Create random graph\n" +
+                                     "3.Display graph\n" +
+                                     "4 Save graph\n" +
+                                     "5.Change Time\n" +
+                                     "6.Change Start Population\n" +
+                                     "7.Set Mutation Index\n" +
+                                     "8.Set Crossbreed Index\n" +
+                                     "9.Set Mutation Method\n" +
+                                     "10.Run Algorithm\n";
+}
+
+namespace PEA3
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ProgramMenu();
+        }
+
+
+        private static void ProgramMenu()
+        {
+            var exit = false;
+            while (!exit)
+            {
+                Console.WriteLine(InfoString.Menu);
+                var input = int.Parse(Console.ReadLine() ?? string.Empty);
+                switch (input)
+                {
+                    case 0:
+                        exit = true;
+                        Console.WriteLine(InfoString.Bye);
+                        break;
+                    case 1:
+                        ProgramManualMenu();
+                        break;
+                    case 2:
+                        Console.WriteLine("TODO Automated");
+                        break;
+                    default:
+                        Console.WriteLine(InfoString.ErrorWrongChoice);
+                        break;
+                }
+            }
+        }
+
+        private static void ProgramManualMenu()
+        {
+            Matrix matrix = null; // field for matrices
+            var exit = false; // boolean for program work
+
+            void DisplayResults(int[] array)
+            {
+                Console.WriteLine("best permutation:");
+                Essentials.DisplayFullArray(array);
+                Console.WriteLine("Road Value:\n" + matrix.CalculateFullRoad(array));
+            }
+            
+            while (!exit)
+            {
+                Console.WriteLine(InfoString.ManualMenu); // print menu
+                var input = int.Parse(Console.ReadLine() ?? string.Empty); // get value from user
+                int[] bestPermutation;
+                switch (input)
+                {
+                    case 0: // exit
+                        exit = true;
+                        break;
+                    case 1: // read given file
+                        Console.Write(InfoString.GetFileName);
+                        matrix = Essentials.ReadFile(Console.ReadLine());
+                        break;
+                    case 2: // create random graph
+                        Console.Write("Write random graph size: ");
+                        var graphSize = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                        matrix = Essentials.GenerateRandomGraph(graphSize);
+                        break;
+                    case 3: // Display matrix
+                        if (matrix == null)
+                        {
+                            Console.WriteLine("ERROR");
+                            break;
+                        }
+
+                        matrix.Display();
+                        break;
+                    case 4: // save to file
+                        Console.Write(InfoString.GetFileName);
+                        var filename = Console.ReadLine();
+                        Essentials.SaveToFile(matrix, filename);
+                        break;
+                    case 5: // change time
+                        break;
+                    case 6:// change start population
+                        break;
+                    case 7:// change mutation index
+                        break;
+                    case 8:// change crossbreed index
+                        break;
+                    case 9:// set mutation method
+                        break;
+                    case 10://genetic algorithm
+                        if (matrix == null)
+                        {
+                            Console.WriteLine(InfoString.ErrorNoMatrix);
+                            break;
+                        }
+
+                        Console.WriteLine("TODO");
+                        break;
+                    default:
+                        Console.WriteLine(InfoString.ErrorWrongChoice);
+                        break;
+                }
+            }
+        }
+    }
+}
