7b37be330055e0f4497e109b6035975730aa25c4
Maciej Byczko <byczqq136@gmail.com>
2021-12-15 23:37:59 +0100

    Fix menu, add coloring, add algorithm module selection

--- PEA3/Program.cs
+++ PEA3/Program.cs
@@ -10,20 +10,21 @@ internal static class InfoString
 
     public const string Menu = "0.Exit\n" +
                                "1.Manual testing\n" +
-                               "2.Automated tests\n";
+                               "2.Automated Tests\n";
 
 
     public const string ManualMenu = "0.Exit\n" +
-                                     "1.Read graph from file\n" +
-                                     "2.Create random graph\n" +
-                                     "3.Display graph\n" +
-                                     "4 Save graph\n" +
+                                     "1.Read Graph From File\n" +
+                                     "2.Create Random Graph\n" +
+                                     "3.Display Graph\n" +
+                                     "4 Save Graph\n" +
                                      "5.Change Time\n" +
-                                     "6.Change Start Population\n" +
-                                     "7.Set Mutation Index\n" +
-                                     "8.Set Crossbreed Index\n" +
-                                     "9.Set Mutation Method\n" +
-                                     "10.Run Algorithm\n";
+                                     "6.Change Population Size\n" +
+                                     "7.Set Mutation Rate\n" +
+                                     "8.Set Crossbreed Rate\n" +
+                                     "9.Change Mutation Method\n" +
+                                     "10.Change Crossbread Method\n" +
+                                     "11.Run Algorithm\n";
 }
 
 namespace PEA3
@@ -66,6 +67,12 @@ namespace PEA3
         {
             Matrix matrix = null; // field for matrices
             var exit = false; // boolean for program work
+            long trialTime = 1;
+            var population = 10;
+            var mutationChance = 0.01;
+            var crossoverChance = 0.8;
+            Action<int[], int, int> mutation = Algorithms.Swap;
+            Func<int[], int[], int[]> crossbreed = Algorithms.PartiallyMatchedCrossover;
 
             void DisplayResults(int[] array)
             {
@@ -73,12 +80,34 @@ namespace PEA3
                 Essentials.DisplayFullArray(array);
                 Console.WriteLine("Road Value:\n" + matrix.CalculateFullRoad(array));
             }
-            
+
+            void PrintParamsValues()
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("=========================");
+                Console.WriteLine($"Time [s]: {trialTime}");
+                Console.WriteLine($"Population Size: {population}");
+                Console.WriteLine($"Mutation Rate: {mutationChance}");
+                Console.WriteLine($"Crossover Rate: {crossoverChance}");
+                Console.Write("Mutation: ");
+                Console.WriteLine(mutation == Algorithms.Swap ? "SWAP" : "REVERSE");
+                Console.Write("Crossover: ");
+                Console.WriteLine(crossbreed == Algorithms.PartiallyMatchedCrossover ? "PMX" : "OX");
+
+                Console.WriteLine(matrix != null
+                    ? $"Loaded matrix size: {matrix.Size}"
+                    : "Loaded matrix size: NULL");
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+            }
+
             while (!exit)
             {
+                PrintParamsValues();
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine(InfoString.ManualMenu); // print menu
+                Console.ResetColor();
                 var input = int.Parse(Console.ReadLine() ?? string.Empty); // get value from user
-                int[] bestPermutation;
                 switch (input)
                 {
                     case 0: // exit
@@ -108,23 +137,48 @@ namespace PEA3
                         Essentials.SaveToFile(matrix, filename);
                         break;
                     case 5: // change time
+                        Console.Write("Write wait time [s]: ");
+                        trialTime = long.Parse(Console.ReadLine() ?? "1");
                         break;
-                    case 6:// change start population
+                    case 6: // change start population
+                        Console.Write("Write permutation size (even value): ");
+                        population = int.Parse(Console.ReadLine() ?? "10");
+                        if (population % 2 != 0)
+                        {
+                            Console.WriteLine("Error,odd population value, fix by adding 1.");
+                            population += 1;
+                        }
+
                         break;
-                    case 7:// change mutation index
+                    case 7: // change mutation chance
+                        mutationChance = double.Parse(Console.ReadLine() ?? "0");
                         break;
-                    case 8:// change crossbreed index
+                    case 8: // change crossbreed chance
+                        crossoverChance = double.Parse(Console.ReadLine() ?? "100");
                         break;
-                    case 9:// set mutation method
+                    case 9: // change mutation method
+                        mutation = mutation == Algorithms.Swap ? Algorithms.Reverse : Algorithms.Swap;
                         break;
-                    case 10://genetic algorithm
+                    case 10: // change crossbreed method
+                        crossbreed = crossbreed == Algorithms.PartiallyMatchedCrossover
+                            ? Algorithms.OrderCrossover
+                            : Algorithms.PartiallyMatchedCrossover;
+                        break;
+                    case 11: //genetic algorithm
                         if (matrix == null)
                         {
                             Console.WriteLine(InfoString.ErrorNoMatrix);
                             break;
                         }
 
-                        Console.WriteLine("TODO");
+                        var bestPermutation = Algorithms.GeneticAlgorithm(matrix,
+                            population,
+                            trialTime,
+                            mutation,
+                            mutationChance,
+                            crossbreed,
+                            crossoverChance);
+                        DisplayResults(bestPermutation);
                         break;
                     default:
                         Console.WriteLine(InfoString.ErrorWrongChoice);
